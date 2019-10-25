697820ebdd4d2b525c5dc4f893081ad2379bc840
Per Haagensen <perhaagensen@Pers-MacBook-Pro.local>
2019-10-25 12:43:02 +0200

    Done  Changes to be committed: 	modified:   style.css 	renamed:    index.html -> todo.html 	deleted:    todo.index 	modified:   todo.js

--- todo.js
+++ todo.js
@@ -1,21 +1,67 @@
-var todoItems = [];
 
+
+//Array which should contain task objects
+//Object consists of a task text and a timestamp
+var tasks = [];
+
+var counter = 0;
+
+//Constructor for making Task-Objects
+function Task(text, timeStamp){
+    this.text = text;
+    this.timeStamp = timeStamp;
+}
+
+
+//Function adds element to list
 function addToList() {
     var element = document.getElementById("todo").value;
 
     if (!(element === "")) {
-        if (!(todoItems.includes(element))) {
             var checkbox = document.createElement("INPUT")
             checkbox.setAttribute("type", "checkbox");
             var node = document.createElement("LI");
+            var label = document.createElement("LABEL");
             var textNode = document.createTextNode(element);
+            label.appendChild(textNode);
             node.appendChild(checkbox);
-            node.appendChild(textNode);
+            node.appendChild(label);
             document.getElementById("lista").appendChild(node);
-            todoItems.push(element);
+            var taskObject = new Task(element, Date.now());
+            tasks.push(taskObject);
+            updateCounter();
+
+            //Added eventlistener for updating counter every time checkbox
+            //is checked!
+            checkbox.addEventListener("change", function(){
+                updateCounter();
+            });
+
         } else {
-            alert("Input already exists or input is empty");
-        }
+            alert("Input is empty");
     }
-    console.log("bash");
+
+
+    //This function updates counters, to keep control over
+    //which tasks has been completed
+    function updateCounter(){
+        let checkBoxList = document.querySelectorAll("input[type=\"checkbox\"]");
+        console.log("checkboxList: " + checkBoxList.length);
+        counter = 0;
+        
+
+        //Iterate over checkBoxlist element
+        //And if the checkbox is checked
+        //update counter by +1
+        for(i of checkBoxList){
+            if(i.checked == true){
+                counter += 1;
+            }
+        }
+
+        document.getElementById("counter").innerHTML  = (counter + "/" + checkBoxList.length + "completed!");
+
+    }
+
+
 }
